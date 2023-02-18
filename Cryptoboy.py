21641b044bc2a3852a7a335e7c9ceddc4d2ea3a3
D-Chamber <timothynnguyen95@gmail.com>
2023-02-18 12:18:15 -0500

    Started Work for making the caesar cipher setup.

---
+++ Cryptoboy.py
@@ -0,0 +1,20 @@
+import string
+
+
+def encrypt(file, key, outfile):
+    alphabet = list(string.ascii_uppercase)
+    print(file, key, outfile)
+
+
+def decrypt(file, key, outfile):
+    alphabet = list(string.ascii_uppercase)
+    print(file, key, outfile)
+
+
+def crack(file, threshold):
+    alphabet = list(string.ascii_uppercase)
+    print(file, threshold)
+
+
+def check():
+    pass
---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+import Cryptoboy
+
+
+class Main:
+    def __init__(self, arg, var1, var2, var3):
+        self.arg = arg
+        self.var1 = var1
+        self.var2 = var2
+        self.var3 = var3
+
+    def run(self):
+        if self.arg == '-e':
+            Cryptoboy.encrypt(self.var1, self.var2, self.var3)
+        elif self.arg == '-d':
+            Cryptoboy.decrypt(self.var1, self.var2, self.var3)
+        elif self.arg == '-c':
+            if self.var2 == '-t':
+                Cryptoboy.crack(self.var1, self.var3)
+            else:
+                print(f'{self.var2} is an invalid input.')
+        else:
+            print(f'{self.arg} is not a valid argument input.')
+
+
+def main():
+    args = sys.argv
+    m = Main(args[1], args[2], args[3], args[4])
+    m.run()
+
+
+if __name__ == '__main__':
+    main()
